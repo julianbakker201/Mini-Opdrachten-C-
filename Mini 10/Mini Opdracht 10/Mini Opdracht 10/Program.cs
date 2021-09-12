8908d32f8c86730a08385ea87660cfabc5982425
julianbakker201 <julbakker201@gmail.com>
2021-09-12 14:47:06 +0200

    tweede commit

---
+++ Mini 10/Mini Opdracht 10/Mini Opdracht 10/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mini_Opdracht_7
+{
+    class IkNeemMee
+    {
+        static void Main(string[] args)
+        {
+                  
+            Console.WriteLine("Vul een woord in en vervolgens moet elk woord beginnen met de laatste letter van het vorige woord.");
+            Console.WriteLine("Heb je genoeg speelt typ dan: stop");
+            Console.WriteLine("Vul nu je eerste voorwerp in. Succes!");
+            Console.WriteLine("Ik ga op vakantie en ik neem mee... ");
+            
+            string woord = Console.ReadLine();
+            char[] woord_array = woord.ToCharArray();
+            var woorden = new List<string>();
+            woorden.Add(woord);
+            Console.WriteLine(woord_array[woord.Length - 1]);
+            while (true)
+            {
+                Console.WriteLine("Je voorwerpen zijn: " + string.Join(", ", woorden));
+                
+                Console.WriteLine("Volgende voorwerp.");
+                
+                string invoer = Console.ReadLine();
+                char[] invoer_array = invoer.ToCharArray();
+                if (invoer.Equals("stop") == true)
+                {
+                    Console.WriteLine("Leuk dat je meedeed.");
+                    break;
+                }
+                else if (invoer_array[0].Equals(woord_array[woord.Length - 1]) == true)
+                {
+                    Console.WriteLine("Goede.");
+                    woord = invoer;
+                    woord_array = woord.ToCharArray();
+                    woorden.Add(invoer);
+                }
+                else if (invoer_array[0].Equals(woord_array[woord.Length - 1]) == false)
+                {
+                    Console.WriteLine(invoer_array[0]);
+                    Console.WriteLine("De begin- en eindletter zijn niet hetzelfde.");
+                    Console.WriteLine("Probeer het nogmaals.");
+
+
+                }
+                
+            }
+        }
+    }
+}
---
+++ Mini 9/Mini Opdracht 9/Mini Opdracht 9/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Mini_Opdracht_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var cessna = new Vliegtuig();
+            Leeuw kaapleeuw = new Leeuw();
+            Spreeuw spreeuwtje = new Spreeuw();
+            cessna.kanVliegen(); 
+        }
+    }
+    public interface IVliegend
+    {
+        void kanVliegen();
+
+    }
+ 
+    public class Vliegtuig : IVliegend
+    {
+        //public Vliegtuig() 
+        //{
+        //   this.kanVliegen = "Ik kan vliegen";
+        //}
+        public void kanVliegen()
+        {
+            Console.WriteLine("Ik kan vliegen");
+        }
+    }
+    public class Leeuw : IVliegend
+    {
+        public void kanVliegen()
+        {
+            Console.WriteLine("Ik kan niet vliegen");
+        }
+
+    }
+    public class Spreeuw : IVliegend
+    {
+        public void kanVliegen()
+        {
+            Console.WriteLine("Ik kan vliegen");
+        }
+    }
+    
+
+}
